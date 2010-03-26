fee338e92c960b62d486aa2232cdd762eef4a9de
David <djlowry@gmail.com>
2010-03-26 15:38:35 +0000

    Template applied

---
+++ template.rb
@@ -0,0 +1,11 @@
+# Template created by You've Got Rails (http://www.youvegotrails.com)
+
+gem 'rubyist-aasm'
+gem 'activemerchant', :lib => 'active_merchant'
+plugin 'paperclip', :git => 'git://github.com/thoughtbot/paperclip.git'
+plugin 'rspec', :git => 'git://github.com/dchelimsky/rspec.git'
+plugin 'rspec-rails', :git => 'git://github.com/dchelimsky/rspec-rails.git'
+plugin 'authlogic', :git => 'git://github.com/binarylogic/authlogic.git'
+# Run rspec generator
+generate("rspec")
+
